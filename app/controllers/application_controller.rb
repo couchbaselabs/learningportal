6ed145689b1947b1db22b8e7b1f453eb47203df6
David Rice <me@davidjrice.co.uk>
2012-04-26 23:17:28 +0100

    Add http auth t:2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate_admin!
+
+  private
+
+  def authenticate_admin!
+    return true if Rails.env.development?
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "couchbase" && password == ENV['HTTP_AUTH_PASSWORD']
+    end
+  end
 end
