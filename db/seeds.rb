4555fe3f2730bebfa1091418730c40a701eb076c
David Rice <me@davidjrice.co.uk>
2012-05-15 14:41:54 +0100

    Reset couch on seed of data t:15

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,8 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
+@couchbase = Couchbase.connect(ENV["COUCHBASE_URL"])
+@couchbase.flush
+@bucket = Couchbase.new(:bucket => 'default')
 
-# TODO couchbase.flush
-# TODO couchbase.new :bucket => 'default'
 Wikipedia.seed!
