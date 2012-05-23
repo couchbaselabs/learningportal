f21827f27a298619286dd229203e7d22ae6a74f3
Mark Leetch <markleetch@gmail.com>
2012-05-23 15:12:11 +0100

    making sure navigation highlights on editing, new pages etc t:15

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -3,7 +3,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.dom_class = 'nav'
     primary.item :dashboard, 'Dashboard', admin_dashboard_path
-    primary.item :articles, 'Articles', admin_articles_path
-    primary.item :users, 'Users', admin_users_path
+    primary.item :articles, 'Articles', admin_articles_path, :highlights_on => /admin\/articles/
+    primary.item :users, 'Users', admin_users_path, :highlights_on => /admin\/users/
   end
 end
