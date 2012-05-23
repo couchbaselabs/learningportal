721ba9e35a3a6aa34299ed4c8c996dd025432efd
Mark Leetch <markleetch@gmail.com>
2012-05-23 17:49:16 +0100

    fixing the users page and modifying the admin nav

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -2,8 +2,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
   navigation.items do |primary|
     primary.dom_class = 'nav'
-    primary.item :dashboard, 'Dashboard', admin_dashboard_path
-    primary.item :articles, 'Articles', admin_articles_path, :highlights_on => /admin\/articles/
+    primary.item :articles, 'Content', admin_articles_path, :highlights_on => /admin\/articles/
     primary.item :users, 'Users', admin_users_path, :highlights_on => /admin\/users/
+    primary.item :dashboard, 'Visit Learning Portal', root_path
   end
 end
