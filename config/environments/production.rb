3bcd4e39c2750c0a6149612ee57747b47c4ded19
David Rice <me@davidjrice.co.uk>
2012-06-07 00:07:31 +0100

    Production cloudfront CDN / asset config for when this is deployed t:10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ LearningPortal::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
+  config.action_controller.asset_host = "//d1cwtzbnfgfzbh.cloudfront.net"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.css )
