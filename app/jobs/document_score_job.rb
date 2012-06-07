cd18bcf5193ce960ec83625babda254c3e9e8c6b
David Rice <me@davidjrice.co.uk>
2012-06-07 03:52:04 +0100

    Update document score calculation job t:15

--- app/jobs/document_score_job.rb
+++ app/jobs/document_score_job.rb
@@ -10,8 +10,12 @@ class DocumentScoreJob
     #    => 100
     max_views = Article.view_stats[:max]
 
-    # 2. what is the amount of views for this document?
-    #    => 1
+    # 2. what is the all time amount of views for this document?
+    document = Article.find("#{@document_id}")
+    total = document['views'] || 0
+    popularity = document['popularity'] || 0
+
+    # 3. what is the recent count of views
     views = 0
     begin
       result = Couch.client(:bucket => "views").get("#{@document_id}")
@@ -19,19 +23,16 @@ class DocumentScoreJob
     rescue Couchbase::Error::NotFound
       # do nothing! we are already 0
     end
-    document = Article.find("#{@document_id}")
 
-    # 3. popularity score is as an integer percentage of the max views (rounded up) between 0..100
-    #    => 1
-    score = 0
-    if views >= max_views
-      score = 100
-    else
-      score = ((views.to_f/max_views.to_f)*100).to_i
-    end
+    # 4. total
+    total = total + views
+    popularity = popularity + views
 
-    # 4. update the document
-    document.update_attributes(:views => views, :popularity => score)
+    # 5. update the document with the new all time total and popularity
+    document.update_attributes(:views => views, :popularity => popularity)
+
+    # 6. reset the counter
+    Couch.client(:bucket => "views").set("#{@document_id}", {:count => 0})
   end
 
 end
