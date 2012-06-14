9587694b4161e9ba42270ede18b8c5d70d4952ed
Kieran Graham <me@kierangraham.com>
2012-06-15 00:52:41 +0100

    re-enabling link to 'content' section of admin dashboard t:2

--- config/admin_navigation.rb
+++ config/admin_navigation.rb
@@ -2,7 +2,7 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
   navigation.items do |primary|
     primary.dom_class = 'nav'
-    #primary.item :articles, 'Content', admin_articles_path, :highlights_on => /admin\/articles/
+    primary.item :articles, 'Content', admin_articles_path, :highlights_on => /admin\/articles/
     primary.item :users, 'Users', admin_users_path, :highlights_on => /admin\/users/
     # primary.item :contributors, 'Contributors', admin_authors_path, :highlights_on => /admin\/authors/
     primary.item :dashboard, 'Visit Learning Portal', root_path
