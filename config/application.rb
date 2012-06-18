97c0f8fdc53ea92f4abb1d1dc7df5c45b184e9c9
David Rice <me@davidjrice.co.uk>
2012-06-18 20:14:02 +0100

    Handle missing output running ruby processes under foreman for local development t:20

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,10 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+# For foreman real time logging
+# https://github.com/ddollar/foreman/wiki/Missing-Output
+$stdout.sync = true if Rails.env.development?
+
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
   Bundler.require(*Rails.groups(:assets => %w(development test)))
