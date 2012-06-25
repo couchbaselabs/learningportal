2a1203c71c57335f2ee492ad6bc4d56c86c9c54a
Kieran Graham <me@kierangraham.com>
2012-06-25 22:15:29 +0100

    fix elasticsearch initializer to point to the correct COUCHBASE_URL t:5

--- config/initializers/elasticsearch_river.rb
+++ config/initializers/elasticsearch_river.rb
@@ -1,2 +1,5 @@
-Typhoeus::Request.put("#{ENV['ELASTIC_SEARCH_URL']}/_river/lp_river/_meta", :body => File.read("app/elasticsearch/river.json"))
+river_body = File.read("app/elasticsearch/river.json")
+river_body.gsub! "COUCHBASE_URL", ENV["COUCHBASE_URL"]
+
+Typhoeus::Request.put("#{ENV['ELASTIC_SEARCH_URL']}/_river/lp_river/_meta", :body => river_body)
 $stdout.puts "Started ElasticSearch river from 'app/elasticsearch/river.json'."
