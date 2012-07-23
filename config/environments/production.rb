c4ca950db07bd67d0395ab045c6a8b7ad08ac867
David Rice <me@davidjrice.co.uk>
2012-07-23 18:16:15 +0100

    Use S3 buckets directly for asset host t:10

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,8 @@ LearningPortal::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "//d1cwtzbnfgfzbh.cloudfront.net"
+  # config.action_controller.asset_host = "//d1cwtzbnfgfzbh.cloudfront.net"
+  config.action_controller.asset_host = "//lp-production.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( admin.css )
