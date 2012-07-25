2fe3978c62b2510fb1726edc428853f617359ac2
David Rice <me@davidjrice.co.uk>
2012-07-25 12:09:04 +0100

    Comment out db/seeds.rb t:2

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,20 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 
-Couch.delete!(:bucket => 'default')
-Couch.delete!(:bucket => 'views')
-Couch.delete!(:bucket => 'profiles')
-Couch.delete!(:bucket => 'system')
-Couch.delete!(:bucket => 'global')
+# Couch.delete!(:bucket => 'default')
+# Couch.delete!(:bucket => 'views')
+# Couch.delete!(:bucket => 'profiles')
+# Couch.delete!(:bucket => 'system')
+# Couch.delete!(:bucket => 'global')
+# 
+# Couch.create!(:bucket => 'default',  :ram => ENV['BUCKET_RAM_DEFAULT'])
+# Couch.create!(:bucket => 'views',    :ram => ENV['BUCKET_RAM_VIEWS'])
+# Couch.create!(:bucket => 'profiles', :ram => ENV['BUCKET_RAM_PROFILES'])
+# Couch.create!(:bucket => 'system',   :ram => ENV['BUCKET_RAM_SYSTEM'])
+# Couch.create!(:bucket => 'global',   :ram => ENV['BUCKET_RAM_GLOBAL'])
+# Couch.create!(:bucket => 'events',   :ram => ENV['BUCKET_RAM_EVENTS'])
 
-Couch.create!(:bucket => 'default',  :ram => ENV['BUCKET_RAM_DEFAULT'])
-Couch.create!(:bucket => 'views',    :ram => ENV['BUCKET_RAM_VIEWS'])
-Couch.create!(:bucket => 'profiles', :ram => ENV['BUCKET_RAM_PROFILES'])
-Couch.create!(:bucket => 'system',   :ram => ENV['BUCKET_RAM_SYSTEM'])
-Couch.create!(:bucket => 'global',   :ram => ENV['BUCKET_RAM_GLOBAL'])
-Couch.create!(:bucket => 'events',   :ram => ENV['BUCKET_RAM_EVENTS'])
-
-Couchbase::Model.ensure_design_document!
+# Couchbase::Model.ensure_design_document!
 # Article.ensure_design_document!
 # Author.ensure_design_document!
 # Category.ensure_design_document!
@@ -23,6 +23,6 @@ Couchbase::Model.ensure_design_document!
 # GlobalViewStats.ensure_design_document!
 
 # Couch.client.flush
-Content.seed!
+# Content.seed!
 
-User.seed!
+# User.seed!
